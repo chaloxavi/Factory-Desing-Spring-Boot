e2a3c121789656660cca02c2af5f5cfe72ce22c9
Gonzalo Daquilema Perez <gonzalodaquilemaperez@Gonzalos-MacBook-Pro.local>
2021-07-11 23:13:17 -0500

    Initial commit

---
+++ src/main/java/com/gonzalo/factory_desing/FactoryDesingApplication.java
@@ -0,0 +1,13 @@
+package com.gonzalo.factory_desing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FactoryDesingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FactoryDesingApplication.class, args);
+	}
+
+}
